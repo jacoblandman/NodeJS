f15803982f6f1a907eda52fcce1ffe39652abd8a
Jacob Landman <jakeypoolandman7@gmail.com>
2017-05-02 11:41:13 -0500

    Finished lesson on es6 classes

---
+++ Events/classes/app.js
@@ -0,0 +1,17 @@
+// only changes how you type something, nothing under the hood
+'use strict';
+
+class Person {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+
+  // automatically put on the prototype
+  greet() {
+    console.log('Hello, ' + this.firstname + ' ' + this.lastname);
+  }
+}
+
+var john = new Person('John', 'Doe');
+john.greet();
