713545e303ffa20adca244cf506d5f69bc4f51ff
Jacob Landman <jakeypoolandman7@gmail.com>
2017-05-02 11:22:25 -0500

    Finished es6 lesson

---
+++ Events/es6/app.js
@@ -0,0 +1,10 @@
+// template literals
+
+var name = 'John Doe';
+var greet = 'Hello ' + name;
+
+// in es6 we can use template literals
+var greet2 = `Hello ${name}`
+
+console.log(greet);
+console.log(greet2);
