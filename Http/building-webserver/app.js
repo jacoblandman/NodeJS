08fa182285f935c51b6d89c5dfb3c152fdb24044
Jacob Landman <jakeypoolandman7@gmail.com>
2017-05-02 15:47:51 -0500

    Learned a little about routing requests

--- Http/building-webserver/app.js
+++ Http/building-webserver/app.js
@@ -5,10 +5,32 @@ var fs = require('fs');
 // this function also returns an object that we can listen to
 http.createServer( function(req, res) {
 
-  // 200 is status, the other object is header info that the browser will understand
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
-  });
-  fs.createReadStream(__dirname + '/index.htm').pipe(res);
+  if (req.url === '/') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    fs.createReadStream(__dirname + '/index.htm').pipe(res)
+  }
+
+  else if (req.url === '/api') {
+    // 200 is status, the other object is header info that the browser will understand
+    // mime type is here
+    res.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    var obj = {
+      firstname: 'John',
+      lastname: 'Doe'
+    };
+
+    res.end(JSON.stringify(obj));
+  }
+
+  else {
+    // could not find
+    res.writeHead(404);
+    res.end();    
+  }
+
 
 }).listen(1337, '127.0.0.1'); // 1337 is the port. The other number is the local ip address
